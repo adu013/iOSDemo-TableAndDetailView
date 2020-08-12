7f12fcfdabbb667ea49db19562a06c32587712f7
adu013 <arindamdutta132@gmail.com>
2020-08-12 22:55:16 +0530

    Initial Commit

---
+++ demo-tableView-detailView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo-tableView-detailView
+//
+//  Created by Ari D on 12/08/20.
+//  Copyright © 2020 Ari D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ demo-tableView-detailViewTests/demo_tableView_detailViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  demo_tableView_detailViewTests.swift
+//  demo-tableView-detailViewTests
+//
+//  Created by Ari D on 12/08/20.
+//  Copyright © 2020 Ari D. All rights reserved.
+//
+
+import XCTest
+@testable import demo_tableView_detailView
+
+class demo_tableView_detailViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
